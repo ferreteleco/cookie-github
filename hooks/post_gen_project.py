ec695f5c6b457975e30871c32eaf609563c5314a
Andrés Ferreiro González <andresoferreiro@gmail.com>
2023-03-12 06:36:36 +0100

    Updates post-gen hooks

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -2,7 +2,7 @@
 
 import logging
 from pathlib import Path
-from shutil import rmtree
+from shutil import rmtree, copy, copytree
 
 
 def change_line_endings_crlf_to_lf():
@@ -26,9 +26,34 @@ def remove_unwanted_templates():
         path.unlink()
 
 
+def add_ci_action_unit_tests_runner():
+    """Adds a CI action for running unit tests, if applicable"""
+
+    target_ci_tests_action = "{{cookiecutter.add_ci_action_unit_tests_runner}}"
+
+    if target_ci_tests_action.lower() != "none":
+
+        LOG.info("Adding action for running unit tests (workflows folder) with default config...")
+
+        destination = Path("workflows")
+        ci_test_workflow_files_path = Path("_", "workflows", target_ci_tests_action)
+        copytree(ci_test_workflow_files_path, destination)
+
+    else:
+        LOG.info("Skipping CI action for running unit tests file generation (%s selected) ...", target_ci_tests_action)
+
+
+def clean():
+    """Remove files and folders only needed as input for generation."""
+    LOG.info("Removing input data folder ...")
+    rmtree("_")
+
+
 if __name__ == "__main__":
 
     logging.basicConfig(level=logging.DEBUG, format="%(message)s")
     LOG = logging.getLogger("post_gen_project")
     remove_unwanted_templates()
+    add_ci_action_unit_tests_runner()
+    clean()
     change_line_endings_crlf_to_lf()
