29405d0efe47a84babf0a4f50a5127be864d3d6d
Andrés Ferreiro González <aferreiro@gradiant.org>
2023-03-20 13:30:31 +0100

    Adds basic test

---
+++ tests/test_basic_cookiecutter_operation.py
@@ -0,0 +1,21 @@
+"""
+test_basic_cookiecutter_operation.py
+
+Maintainer Andrés Ferreiro González (andres.ferreiro.glez@gmail.com)
+Created @ Monday, 20th March 2023 1:27:53 pm
+
+Copyright (c) 2023 Andrés Ferreiro González
+All Rights Reserved
+"""
+
+
+def test_bake_folder(cookies):
+    """Basic cookiecutter test for baking a .github folder with default settings"""
+
+    result = cookies.bake()
+
+    assert result.exit_code == 0
+    assert result.exception is None
+
+    assert result.project_path.name == ".github"
+    assert result.project_path.is_dir()
